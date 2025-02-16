5a79d313a2b323a1e03422d0c420ebef0c5b14ce
ScrachStack <122732007+ScrachStack@users.noreply.github.com>
2025-02-16 16:44:36 -0600

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+games { 'rdr3', 'gta5' }
+
+author 'ScrachStack'
+description 'https://github.com/ScrachStack/loading-screen'
+version '1.0.0'
+
+client_scripts {
+    'client.lua'
+}
+
+loadscreen 'html/index.html'
+file 'html/index.html'
+
